463ce33dd94db18efc3826870ec8c680002f0c05
SUDIPP22 <panja370@gmail.com>
2021-08-06 04:38:57 +0530

    Added : To get a Dice Number Randomly

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,8 +2,11 @@ package com.bridgelabz;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int player = 0;
+        int playerPosition = 0;       // Initial Position
         System.out.println("Welcome To Snake And Ladder Game");
-        System.out.println("Your Position is : " +player);
+        System.out.println("Your Initial Position is : " + playerPosition);
+
+        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);   // For Generating 1 to 6 Number Randomly
+        System.out.println("After Die Roll You get Number : " + dieRoll);
     }
 }
