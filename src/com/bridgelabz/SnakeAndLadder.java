a31be4b99732b965c9652a19cb5d4f0f646d331c
SUDIPP22 <panja370@gmail.com>
2021-08-06 05:27:08 +0530

    Added : Checking for options to play

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -8,5 +8,19 @@ public class SnakeAndLadder {
 
         int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);   // For Generating 1 to 6 Number Randomly
         System.out.println("After Die Roll You get Number : " + dieRoll);
+
+        // Constants for checking the options
+        int NO_PLAY = 0;
+        int LADDER = 1;
+        int SNAKE = 2;
+        int optionCheck = (int) (Math.random() * 3);    // To check for options
+
+        // Condition checking
+        if (optionCheck == NO_PLAY) {
+            System.out.println("You stays in " + playerPosition + " position ");
+        } else if (optionCheck == LADDER) {
+            System.out.println("You get the Ladder and moves ahead by " + dieRoll + " steps");
+        } else
+            System.out.println("You get the Snake and moves behind by " + dieRoll + " steps");
     }
 }
