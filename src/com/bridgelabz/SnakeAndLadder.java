c0490cb17662a9225f6183ed75344bccf030eb9a
SUDIPP22 <panja370@gmail.com>
2021-08-07 17:41:32 +0530

    Refactored : Player reaches the winning position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,37 +1,49 @@
 package com.bridgelabz;
 
 public class SnakeAndLadder {
+
+    public static final int WINNING_POSITION = 100;
+    public static int PLAYER_POSITION = 0;
+
     public static void main(String[] args) {
-        int playerPosition = 0;       // Initial Position
-        System.out.println("Welcome To Snake And Ladder Game");
-        System.out.println("Your Initial Position is : " + playerPosition);
+        System.out.println("******Welcome To Snake And Ladder Game*******");
+        checkWinner();
+    }
 
-        int dieRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);   // For Generating 1 to 6 Number Randomly
-        System.out.println("After Die Roll You get Number : " + dieRoll);
+    public static void checkWinner() {
+        while (PLAYER_POSITION < WINNING_POSITION) {
+            int dieRoll = (int) (Math.random() * 6 + 1);   // For Generating 1 to 6 Number Randomly
+            System.out.println("\n<---->After Die Roll You get Number : " + dieRoll);
 
-        // Constants for checking the options
-        int NO_PLAY = 0;
-        int LADDER = 1;
-        int SNAKE = 2;
-        int newPlayerPosition;
-        int optionCheck = (int) (Math.random() * 3);    // To check for options
+            int optionCheck = (int) (Math.random() * 3);    // To check for options
 
-        // Condition checking
+            // variables
+            int noPlay = 0;
+            int ladder = 1;
+            int snake = 2;
 
-        switch (optionCheck) {
-            case 0:
-                System.out.println("You stays in " + playerPosition + " position ");
-                break;
-            case 1:
-                System.out.println("You get the Ladder and moves ahead by " + dieRoll + " steps");
-                newPlayerPosition = playerPosition + dieRoll;
-                System.out.println("Now your position is : " + newPlayerPosition);
-                break;
-            case 2:
-                System.out.println("You get the Snake and moves behind by " + dieRoll + " steps");
-                newPlayerPosition = playerPosition - dieRoll;
-                System.out.println("Now your position is : " + newPlayerPosition);
-                break;
+            // Condition checking
+            switch (optionCheck) {
+                case 0:
+                    System.out.println("It is a No-Play " + " :(  " + ":("); // PLAYER_POSITION IS 0
+
+                    break;
+                case 1:
+                    System.out.println("You got Ladder" + " /-/ ");
+                    PLAYER_POSITION += dieRoll;
+                    break;
+                case 2:
+                    System.out.println("You got Snake " + " $$ ");
+                    PLAYER_POSITION -= dieRoll;
+
+                    if (PLAYER_POSITION < 0) {
+                        System.out.println("You have to start from 0 " + " :( " + ":(");
+                        PLAYER_POSITION = 0;
+                    }
+                    break;
+            }
+            System.out.println("your position is :" + PLAYER_POSITION);
         }
+        System.out.println("you win the game  :) :)");
     }
 }
