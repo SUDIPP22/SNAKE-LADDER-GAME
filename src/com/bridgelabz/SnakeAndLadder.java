d07c53cd7016f8c62a2612e2ad00f5fd723951a3
SUDIPP22 <panja370@gmail.com>
2021-08-08 23:03:25 +0530

    Refactored : Playing the Game with two player

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,55 +1,99 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class SnakeAndLadder {
 
     public static final int WINNING_POSITION = 100;
-    public static int PLAYER_POSITION = 0;
 
     public static void main(String[] args) {
         System.out.println("******Welcome To Snake And Ladder Game*******");
-        checkWinner();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ENTER YOUR NAME " + " PLAYER1 ");
+        String firstPlayerName = sc.next();                         // for player1
+        int positionOfPlayer1 = 0;
+
+        System.out.println("ENTER YOUR NAME : " + " PLAYER2 ");
+        String secondPlayerName = sc.next();                       // for player2
+        int positionOfPlayer2 = 0;
+        sc.close();
+
+        // Checking WINNER
+        while (positionOfPlayer1 <= WINNING_POSITION && positionOfPlayer2 <= WINNING_POSITION) {
+            if (positionOfPlayer1 == WINNING_POSITION) {
+                System.out.println("\nCongratulations " + " :) " + ":) " + firstPlayerName + ", You Won The Game!!!! ");
+                break;
+            } else if (positionOfPlayer2 == WINNING_POSITION) {
+                System.out.println("\nCongratulations " + " :) " + ":) " + secondPlayerName + ", You Won The Game!!!! ");
+                break;
+            } else {
+                positionOfPlayer1 = checkWinnerForPlayer1(positionOfPlayer1, firstPlayerName);
+                positionOfPlayer2 = checkWinnerForPlayer2(positionOfPlayer2, secondPlayerName);
+            }
+        }
     }
 
-    public static void checkWinner() {
-        int numOfDieRoll = 0;                              // input variable for counting die rolling
-        while (PLAYER_POSITION < WINNING_POSITION) {
-            int dieRoll = (int) (Math.random() * 6 + 1);   // For Generating 1 to 6 Number Randomly
-            numOfDieRoll++;                                // method for counting die rolling
-            System.out.println("\n<---->After Die Roll You get Number : " + dieRoll);
+    private static int checkWinnerForPlayer1(int positionOfPlayer1, String firstPlayerName) {
+        int dieRoll = (int) (Math.random() * 6 + 1);   // For Generating 1 to 6 Number Randomly
+        System.out.println("\n<---->After Die Roll You get Number : " + dieRoll);
+        int optionCheck = (int) (Math.random() * 3);    // To check for options
+        int forExactWin = positionOfPlayer1 + dieRoll;
 
-            int optionCheck = (int) (Math.random() * 3);    // To check for options
-
-            // variables
-            int noPlay = 0;
-            int ladder = 1;
-            int snake = 2;
-            int forExactWin = PLAYER_POSITION + dieRoll;
-
-            // Condition checking
-            switch (optionCheck) {
-                case 0:
-                    System.out.println("It is a No-Play " + " :(  " + ":("); // PLAYER_POSITION IS 0
-                    break;
-                case 1:
-                    System.out.println("You got Ladder" + " /-/ ");
-                    if (forExactWin > 100) {                           // condition for exact winning position
-                        PLAYER_POSITION = PLAYER_POSITION;
-                    } else
-                        PLAYER_POSITION += dieRoll;
-                    break;
-                case 2:
-                    System.out.println("You got Snake " + " $$ ");
-                    PLAYER_POSITION -= dieRoll;
-
-                    if (PLAYER_POSITION < 0) {
-                        System.out.println("You have to start from 0 " + " :( " + ":(");
-                        PLAYER_POSITION = 0;
-                    }
-                    break;
-            }
-            System.out.println("your position is :" + PLAYER_POSITION);  // to check the position of every die roll
+        // Condition checking
+        switch (optionCheck) {
+            case 0:
+                System.out.println("It is a No-Play " + " :(  " + ":("); // PLAYER_POSITION IS 0
+                break;
+            case 1:
+                System.out.println("You got Ladder" + " /-/ ");
+                if (forExactWin > 100) {                           // condition for exact winning position
+                    positionOfPlayer1 = positionOfPlayer1;
+                } else
+                    positionOfPlayer1 += dieRoll;
+                break;
+            case 2:
+                System.out.println("You got Snake " + " $$ ");
+                positionOfPlayer1 -= dieRoll;
+                if (positionOfPlayer1 < 0) {
+                    System.out.println("You have to start from 0 " + " :( " + ":(");
+                    positionOfPlayer1 = 0;
+                }
+                break;
         }
-        System.out.println("\nNumber of times the dice was played to win the game is : " + numOfDieRoll);
-        System.out.println("\nYou win the game  :) :)");
+        System.out.println(firstPlayerName + " Your position is :" + positionOfPlayer1);  // to check the position of every die roll
+        return positionOfPlayer1;
+    }
+
+    private static int checkWinnerForPlayer2(int positionOfPlayer2, String secondPlayerName) {
+        int dieRoll = (int) (Math.random() * 6 + 1);   // For Generating 1 to 6 Number Randomly
+        System.out.println("\n<---->After Die Roll You get Number : " + dieRoll);
+        int optionCheck = (int) (Math.random() * 3);    // To check for options
+        int forExactWin = positionOfPlayer2 + dieRoll;
+
+        // Condition checking
+        switch (optionCheck) {
+            case 0:
+                System.out.println("It is a No-Play " + " :(  " + ":(");
+                break;
+            case 1:
+                System.out.println("You got Ladder" + " /-/ ");
+                if (forExactWin > 100) {                           // condition for exact winning position
+                    positionOfPlayer2 = positionOfPlayer2;
+                } else
+                    positionOfPlayer2 += dieRoll;
+                break;
+            case 2:
+                System.out.println("You got Snake " + " $$ ");
+                positionOfPlayer2 -= dieRoll;
+
+                if (positionOfPlayer2 < 0) {
+                    System.out.println("You have to start from 0 " + " :( " + ":(");
+                    positionOfPlayer2 = 0;
+                }
+                break;
+        }
+        System.out.println(secondPlayerName + " Your position is :" + positionOfPlayer2);  // to check the position of every die roll
+        return positionOfPlayer2;
     }
 }
